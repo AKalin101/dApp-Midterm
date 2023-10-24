ccede0b2b3487ee0c432e47fda5c6d1e371bfb70
AKalin101 <146862207+AKalin101@users.noreply.github.com>
2023-10-24 09:27:11 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,129 +1,143 @@
-// Check if Web3 has been injected by the browser (Mist/MetaMask)
-if (typeof web3 !== 'undefined') {
-    web3 = new Web3(web3.currentProvider);
-  } else {
-    // Set the provider you want from Web3.providers (e.g. if you're using Ganache for local development)
-    web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-  }
+// // Check if Web3 has been injected by the browser (Mist/MetaMask)
+// if (typeof web3 !== 'undefined') {
+//     web3 = new Web3(web3.currentProvider);
+//   } else {
+//     // Set the provider you want from Web3.providers (e.g. if you're using Ganache for local development)
+//     web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+//   }
   
-  // Check if user is connected to Ethereum
-  web3.eth.getAccounts(function(err, accounts){
-    if (err != null) {
-        alert("Error retrieving accounts.");
-        return;
-    }
-    if (accounts.length === 0) {
-        alert("No accounts found. Make sure Ethereum client is connected.");
-        return;
-    }
+//   // Check if user is connected to Ethereum
+//   web3.eth.getAccounts(function(err, accounts){
+//     if (err != null) {
+//         alert("Error retrieving accounts.");
+//         return;
+//     }
+//     if (accounts.length === 0) {
+//         alert("No accounts found. Make sure Ethereum client is connected.");
+//         return;
+//     }
   
-    let account = accounts[0]; // The first account
+//     let account = accounts[0]; // The first account
   
     // Contract ABI (from your Solidity contract, can be generated using Remix or truffle compile)
-    let abi = [
-        {
-          "anonymous": false,
-          "inputs": [
-            {
-              "indexed": true,
-              "internalType": "address",
-              "name": "voter",
-              "type": "address"
-            }
-          ],
-          "name": "VotedPlayStation",
-          "type": "event"
-        },
-        {
-          "anonymous": false,
-          "inputs": [
-            {
-              "indexed": true,
-              "internalType": "address",
-              "name": "voter",
-              "type": "address"
-            }
-          ],
-          "name": "VotedXbox",
-          "type": "event"
-        },
-        {
-          "inputs": [],
-          "name": "votePlayStation",
-          "outputs": [],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        },
-        {
-          "inputs": [],
-          "name": "voteXbox",
-          "outputs": [],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        },
-        {
-          "inputs": [],
-          "name": "getVotes",
-          "outputs": [
-            {
-              "internalType": "uint256",
-              "name": "playstation",
-              "type": "uint256"
-            },
-            {
-              "internalType": "uint256",
-              "name": "xbox",
-              "type": "uint256"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "address",
-              "name": "",
-              "type": "address"
-            }
-          ],
-          "name": "hasVoted",
-          "outputs": [
-            {
-              "internalType": "bool",
-              "name": "",
-              "type": "bool"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [],
-          "name": "votes",
-          "outputs": [
-            {
-              "internalType": "uint256",
-              "name": "playstationVotes",
-              "type": "uint256"
-            },
-            {
-              "internalType": "uint256",
-              "name": "xboxVotes",
-              "type": "uint256"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        }
-    ]
+    // let abi = [
+    //     {
+    //       "anonymous": false,
+    //       "inputs": [
+    //         {
+    //           "indexed": true,
+    //           "internalType": "address",
+    //           "name": "voter",
+    //           "type": "address"
+    //         }
+    //       ],
+    //       "name": "VotedPlayStation",
+    //       "type": "event"
+    //     },
+    //     {
+    //       "anonymous": false,
+    //       "inputs": [
+    //         {
+    //           "indexed": true,
+    //           "internalType": "address",
+    //           "name": "voter",
+    //           "type": "address"
+    //         }
+    //       ],
+    //       "name": "VotedXbox",
+    //       "type": "event"
+    //     },
+    //     {
+    //       "inputs": [],
+    //       "name": "votePlayStation",
+    //       "outputs": [],
+    //       "stateMutability": "nonpayable",
+    //       "type": "function"
+    //     },
+    //     {
+    //       "inputs": [],
+    //       "name": "voteXbox",
+    //       "outputs": [],
+    //       "stateMutability": "nonpayable",
+    //       "type": "function"
+    //     },
+    //     {
+    //       "inputs": [],
+    //       "name": "getVotes",
+    //       "outputs": [
+    //         {
+    //           "internalType": "uint256",
+    //           "name": "playstation",
+    //           "type": "uint256"
+    //         },
+    //         {
+    //           "internalType": "uint256",
+    //           "name": "xbox",
+    //           "type": "uint256"
+    //         }
+    //       ],
+    //       "stateMutability": "view",
+    //       "type": "function"
+    //     },
+    //     {
+    //       "inputs": [
+    //         {
+    //           "internalType": "address",
+    //           "name": "",
+    //           "type": "address"
+    //         }
+    //       ],
+    //       "name": "hasVoted",
+    //       "outputs": [
+    //         {
+    //           "internalType": "bool",
+    //           "name": "",
+    //           "type": "bool"
+    //         }
+    //       ],
+    //       "stateMutability": "view",
+    //       "type": "function"
+    //     },
+    //     {
+    //       "inputs": [],
+    //       "name": "votes",
+    //       "outputs": [
+    //         {
+    //           "internalType": "uint256",
+    //           "name": "playstationVotes",
+    //           "type": "uint256"
+    //         },
+    //         {
+    //           "internalType": "uint256",
+    //           "name": "xboxVotes",
+    //           "type": "uint256"
+    //         }
+    //       ],
+    //       "stateMutability": "view",
+    //       "type": "function"
+    //     }
+    // ]
   
-    // Contract address (replace with the address after deploying your contract)
-    let contractAddress = "0x7E23B900576423eFe5F69944dc94cF836f42CF36";
+    // // Contract address (replace with the address after deploying your contract)
+    // let contractAddress = "0x7E23B900576423eFe5F69944dc94cF836f42CF36";
   
-    // Create a new contract instance with the provided ABI and address
-    let consoleVoteContract = new web3.eth.Contract(abi, contractAddress);
+    // // Create a new contract instance with the provided ABI and address
+    // let consoleVoteContract = new web3.eth.Contract(abi, contractAddress);
   
+    //Dougs Code 
+    let contract;
+    let signer;
+    let contractWithSigner;
+    
+    main();
+    
+    async function main() {
+        const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+        await provider.send("eth_requestAccounts", []);
+        signer = provider.getSigner();
+        contract = new ethers.Contract(contractAddress, contractABI, provider);
+        contractWithSigner = contract.connect(signer);
+    
     $('#voteForPlayStation').on("click", function() {
       voteForPlayStation()
     })
@@ -161,4 +175,4 @@ if (typeof web3 !== 'undefined') {
             console.log("Xbox Votes: ", result[1]);
         });
     }
-  });
+  };
